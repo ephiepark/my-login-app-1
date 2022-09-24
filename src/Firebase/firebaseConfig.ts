74051cdd8178b3634dcba9a17da865b105d44da0
Ephraim Park <ephiepark@gmail.com>
2022-09-24 16:32:40 -0700

    Connect with a firebase project

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDsiTn1tYDcIpngdCCxQJANtr_e6H26mj0",
+  authDomain: "my-login-app-1.firebaseapp.com",
+  projectId: "my-login-app-1",
+  storageBucket: "my-login-app-1.appspot.com",
+  messagingSenderId: "715661451472",
+  appId: "1:715661451472:web:32329f867f2ca32c737ed5",
+  measurementId: "G-XRVCNVJJ3D"
+};
